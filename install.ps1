06c82c7271988c84b97f7d77d18e32eb3a60a152
Confusingboat <confusingboat@gmail.com>
2020-07-13 19:48:24 -0500

    init

---
+++ install.ps1
@@ -0,0 +1,17 @@
+# Hyper-V for Docker
+enable-windowsoptionalfeature -online -featurename microsoft-hyper-v -all
+
+# Chocolatey for everything
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+refreshenv
+
+choco install slack -y
+choco install discord -y
+choco install spotify -y
+choco install microsoft-edge -y
+choco install brave -y
+choco install 1password -y
+choco install docker-desktop -y
+choco install vscode --params "/NoDesktopIcon" -y
+choco install visualstudio2019community -y
+choco install resharper-ultimate-all --params "'/PerMachine /NoCpp /NoTeamCityAddin'" -y

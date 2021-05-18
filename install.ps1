84eb907b8d3cda37faf8e32ad812057fe11a16d6
Confusingboat <confusingboat@gmail.com>
2021-05-18 10:19:43 -0700

    add wsl2 for docker desktop

--- install.ps1
+++ install.ps1
@@ -5,6 +5,13 @@ enable-windowsoptionalfeature -online -featurename microsoft-hyper-v -all
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 refreshenv
 
+# Setup WSL2
+dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
+dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
+wget https://wslstorestorage.blob.core.windows.net/wslblob/wsl_update_x64.msi -outfile "$(get-location)\wsl_update_x64.msi"
+msiexec /i wsl_update_x64.msi /quiet
+wsl --set-default-version 2
+
 choco install slack -y
 choco install discord -y
 choco install spotify -y
